58dbe5ecaf47b9c929123fb013be7250fe856674
nicoagarcia <nicoagarcia1@gmail.com>
2023-03-15 16:18:47 -0300

    bot

---
+++ bot.py
@@ -0,0 +1,31 @@
+import time
+from datetime import datetime
+from tkinter import messagebox
+from playsound import playsound
+from playwright.sync_api import Playwright, sync_playwright
+
+def get_link(page):
+    eventoArgentina = page.locator('xpath=/html/body/div[4]/div/div[2]/div/div/div/div[1]/div/a')
+    link = eventoArgentina.get_attribute('href')
+    return link
+
+with sync_playwright() as playwright:
+    browser = playwright.chromium.launch()
+    context = browser.new_context()
+    page = context.new_page()
+    page.goto("https://www.deportick.com/")
+
+    while True:
+        link = get_link(page)
+        if link:
+            print("\nHora: {}\nFlaquito, activa, se publicaron! sos bueno esperando!\n".format(datetime.fromtimestamp(time.time())))
+            playsound('PvZ Victory Jingle.mp3')
+            messagebox.showinfo(message="La imagen tiene un link asociado!", title="ATENCION")
+            print(link)
+            break
+        else:
+            print("\nHora: {}\nNunca vi a nadie, que espere tan bien las entradas como vos. pero sigue sin publicarse\n".format(datetime.fromtimestamp(time.time())))
+            time.sleep(60)
+
+    context.close()
+    browser.close()
